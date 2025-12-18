f97b531bc019cd5198c0f23a623cd74e1dcd3faf
HawkOsm <osmansahinguler@gmail.com>
2025-12-18 15:01:03 +0300

    Basics are ready and alpha model is starting tp be uploaded

---
+++ src/sections/About.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
+
+import Button from '../components/Button.jsx';
+
+const About = () => {
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('osmansahinguler@gmail.com')
+            .then(() => {
+                setHasCopied(true);
+                setTimeout(() => {
+                    setHasCopied(false);
+                }, 2000);
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
+    };
+
+    return (
+        <section className="c-space my-20" id="about">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="public/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">Hi, I’m Osman Şahin</p>
+                            <p className="grid-subtext">
+                                I am a second-year Software Engineering student with a background in object detection.
+                                I enjoy learning by building projects and solving real-world problems.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="public/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />//fix
+
+                        <div>
+                            <p className="grid-headtext">Tech Stack</p>
+                            <p className="grid-subtext">
+                                I enjoy working with different languages, frameworks, and tools to turn ideas into real, working applications.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-1 xl:row-span-4">
+                    <div className="grid-container">
+                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0, 0, 0, 0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                            />
+                        </div>
+                        <div>
+                            <p className="grid-headtext">Looking for opportunity</p>
+                            <p className="grid-subtext">I&apos;m based in Turkey, Izmir and open to remote work and internship opportunities in worldwide.</p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="xl:col-span-2 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="public/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+                        <div>
+                            <p className="grid-headtext">My Passion for Coding</p>
+                            <p className="grid-subtext">
+                                I love solving problems and building things through code. Programming isn&apos;t just my
+                                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img
+                            src="public/assets/grid4.png"
+                            alt="grid-4"
+                            className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+                        />
+
+                        <div className="space-y-2">
+                            <p className="grid-subtext text-center">Contact me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCopied ? 'public/assets/tick.svg' : 'public/assets/copy.svg'} alt="copy" />
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">osmansahinguler@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
